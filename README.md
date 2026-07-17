e98fb3d7b17e188c039e35d50daa9b04dbb50be9
MaxwellJChen <maxwelljchen1@gmail.com>
2026-07-16 23:12:34 -0500

    feat: added Prisma configuration to deployment script and additional shadcn/ui styling

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Habits
-Track habits and analytics.
+# Intent
+Track habits and their analytics.
+
+## Backlog
+- [x] Log in with email and password
+- [x] Log in with Google
+- [x] Set up PostgreSQL database with Prisma
+- [x] Connect PostgreSQL database to Better Auth
+- [ ] Verify emails
+- [ ] Initial deployment on Vercel
+- [ ] Sync users across multiple login methods
+- [ ] Style with shadcn/ui
+
+## Issues
+- [ ] Currently using a Google Group with support email not having @intent.ac domain
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,25 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@/lib/prisma";
+import { Resend } from "resend";
+
+// const resend = new Resend()
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, { provider: "postgresql" }),
   emailAndPassword: {
     enabled: true,
+    requireEmailVerification: true,
+  },
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url }) => {
+      await sendEmail({
+        to: user.email,
+        subject: "Verify your email address",
+        text: `Click the link to verify your email: ${url}`,
+      });
+    },
+    sendOnSignUp: true,
   },
   socialProviders: {
     github: {

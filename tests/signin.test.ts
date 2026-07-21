4600e894f7051d560c7f7afcf2e0682838af85be
MaxwellJChen <maxwelljchen1@gmail.com>
2026-07-21 03:47:34 -0500

    test: added initial test for sign in page

--- tests/signin.test.ts
+++ tests/signin.test.ts
@@ -1,1 +1,22 @@
 import { test, expect } from "@playwright/test";
+
+test.describe("page structure", () => {
+  test("page should have correct elements and metadata", async ({ page }) => {
+    await page.goto("/signin");
+
+    await expect(
+      page.getByRole("heading", { name: "Sign In", exact: true }),
+    ).toBeVisible();
+
+    await expect(page.getByPlaceholder("Email")).toBeVisible();
+    await expect(page.getByPlaceholder("Password")).toBeVisible();
+
+    await expect(
+      page.getByRole("button", { name: "Sign In", exact: true }),
+    ).toBeVisible();
+    await expect(page.getByRole("button", { name: "Google" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "GitHub" })).toBeVisible();
+
+    await expect(page.getByRole("link", { name: "sign up" })).toBeVisible();
+  });
+});
